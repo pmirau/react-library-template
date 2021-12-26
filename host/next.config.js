1e263ffc5c558230ac7a1f18aed367c66d299701
Philip Mirau <philip.mirau@gmail.com>
2021-12-26 16:12:17 +0100

    build: fix NextJS link error
    - `Invalid hook call` because of multiple react versions

--- host/next.config.js
+++ host/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: require.resolve('react').replace('index.js', ''),
+    };
+
+    return config;
+  },
 }
