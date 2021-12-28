61198d6b6b3cc13274fcc59ddca789e5c6b6f25b
Philip Mirau <philip.mirau@gmail.com>
2021-12-28 21:04:49 +0100

    fix: add prop-types to next.config.js
    * Reason: NextJS threw `Module not found: Can't resolve 'prop-types'`

--- host/next.config.js
+++ host/next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     config.resolve.alias = {
       ...config.resolve.alias,
       react: require.resolve('react').replace('index.js', ''),
+      ['prop-types']: require.resolve('prop-types').replace('index.js', ''),
     };
 
     return config;
